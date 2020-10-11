2b9ffa77bdad530ffdbf8cb148704eb106f0c271
cn808 <siyuanpfx@gmail.com>
2020-10-10 14:24:23 -1000

    Cleaned up files

--- StringSplosion.java
+++ StringSplosion.java
@@ -1,19 +1,29 @@
-/*
-https://codingbat.com/prob/p117334
+package GoogleTechDev;
 
-Given a non-empty string like "Code" return a string like "CCoCodCode".
-stringSplosion("Code") → "CCoCodCode"
-stringSplosion("abc") → "aababc"
-stringSplosion("ab") → "aab"
-*/
+public class StringSplosion {
+  /*
+    https://codingbat.com/prob/p117334
 
-public String stringSplosion(String str) {
-  if(str == null || str.length() == 0) return str;
+    Given a non-empty string like "Code" return a string like "CCoCodCode".
+    stringSplosion("Code") → "CCoCodCode"
+    stringSplosion("abc") → "aababc"
+    stringSplosion("ab") → "aab"
+  */
 
-  StringBuilder sb = new StringBuilder();
-  for(int i = 0; i < str.length(); i++) {
-    sb.append(str.substring(0, i+1));
+  public static String stringSplosion(String str) {
+    if(str == null || str.length() == 0) return str;
+
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < str.length(); i++) {
+      sb.append(str.substring(0, i+1));
+    }
+
+    return sb.toString();
   }
 
-  return sb.toString();
+  public static void main(String[] args) {
+    System.out.println(stringSplosion("Code"));
+    System.out.println(stringSplosion("abc"));
+    System.out.println(stringSplosion("ab"));
+  }
 }
--- WithoutString.java
+++ WithoutString.java
@@ -1,45 +1,55 @@
-/*
-https://codingbat.com/prob/p192570
+package GoogleTechDev;
 
-Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+public class WithoutString {
+  /*
+    https://codingbat.com/prob/p192570
 
-withoutString("Hello there", "llo") → "He there"
-withoutString("Hello there", "e") → "Hllo thr"
-withoutString("Hello there", "x") → "Hello there"
-*/
+    Given two strings, base and remove, return a version of the base string where all instances of the remove string have been removed (not case sensitive). You may assume that the remove string is length 1 or more. Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
 
-public String withoutString(String base, String remove) {
-  if(base == null || base.length() == 0 || 
-    remove == null || remove.length() == 0) return base;
-  
-  char[] baseCharArray = base.toLowerCase().toCharArray();
-  char[] removeCharArray = remove.toLowerCase().toCharArray();
-  StringBuilder sb = new StringBuilder();
-  
-  int baseIndex = 0;
-  while(baseIndex < base.length()) {
-    int removeIndex = 0;
-    int temp = baseIndex;
-    while(removeIndex < remove.length() && 
-          temp < base.length() && 
-          baseCharArray[temp] == removeCharArray[removeIndex]) 
-    {
-          temp++;
-          removeIndex++;
+    withoutString("Hello there", "llo") → "He there"
+    withoutString("Hello there", "e") → "Hllo thr"
+    withoutString("Hello there", "x") → "Hello there"
+  */
+
+  public static String withoutString(String base, String remove) {
+    if(base == null || base.length() == 0 || 
+      remove == null || remove.length() == 0) return base;
+    
+    char[] baseCharArray = base.toLowerCase().toCharArray();
+    char[] removeCharArray = remove.toLowerCase().toCharArray();
+    StringBuilder sb = new StringBuilder();
+    
+    int baseIndex = 0;
+    while(baseIndex < base.length()) {
+      int removeIndex = 0;
+      int temp = baseIndex;
+      while(removeIndex < remove.length() && 
+            temp < base.length() && 
+            baseCharArray[temp] == removeCharArray[removeIndex]) 
+      {
+            temp++;
+            removeIndex++;
+      }
+      
+      if(removeIndex >= remove.length()) {
+        baseIndex += removeIndex;
+        continue;
+      }
+      
+      if(baseIndex == base.length()) {
+        sb.append(base.substring(baseIndex, baseIndex));
+      } else {
+        sb.append(base.substring(baseIndex, baseIndex+1));
+      }
+      baseIndex++;
     }
     
-    if(removeIndex >= remove.length()) {
-      baseIndex += removeIndex;
-      continue;
-    }
-    
-    if(baseIndex == base.length()) {
-      sb.append(base.substring(baseIndex, baseIndex));
-    } else {
-      sb.append(base.substring(baseIndex, baseIndex+1));
-    }
-    baseIndex++;
+    return sb.toString();
   }
-  
-  return sb.toString();
+
+  public static void main(String[] args) {
+    System.out.println(withoutString("Hello there", "llo"));
+    System.out.println(withoutString("Hello there", "e"));
+    System.out.println(withoutString("Hello there", "x"));
+  }
 }
